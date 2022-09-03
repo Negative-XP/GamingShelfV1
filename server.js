fd59f4a79319b747b9d021b8412abf1521483f80
Austin Pedigo <pedigo.was.here@gmail.com>
2022-09-03 09:39:30 -0400

    Updated

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(flash())
   
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
- 
+
 app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
+    console.log(`Server is running on port ${process.env.PORT}, you better catch it!`)
 })    
