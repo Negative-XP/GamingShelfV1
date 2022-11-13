ea4993c0f84de28ce28122d64cadf78460cec639
Austin Pedigo <pedigo.was.here@gmail.com>
2022-11-13 12:57:50 -0500

    Had to fix the delete function on website

--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,23 @@ const upload = require("../middleware/multer");
 const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
+
+router.use( function( req, res, next ) {
+    // this middleware will call for each requested
+    // and we checked for the requested query properties
+    // if _method was existed
+    // then we know, clients need to call DELETE request instead
+    if ( req.query._method == 'DELETE' ) {
+        // change the original METHOD
+        // into DELETE method
+        req.method = 'DELETE';
+        // and set requested url to /user/12
+        req.url = req.path;
+    }       
+    next(); 
+});
+
+
 router.get("/:id", ensureAuth, postsController.getPost);
 
 router.post("/createPost/:id", upload.single("file"), postsController.createPost);
